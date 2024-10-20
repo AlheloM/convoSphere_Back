01a4e3281fbe681af43148ed534ffa724cb258e7
MuathAlsabbah <muath.alsabbah@gmail.com>
2024-10-20 14:15:17 +0300

    h

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(logger('dev'));
+app.use('auth')
 
 // Existing issue routes...
 app.get('/issues', async (req, res) => {
